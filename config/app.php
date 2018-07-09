55d0c14b80649310568d5a4698a4acc5fe3aa9b3
Ragulan <Ragulan28@gmail.com>
2018-07-09 19:37:07 +0530

    Socialite configured

--- config/app.php
+++ config/app.php
@@ -151,6 +151,8 @@ return [
          * Package Service Providers...
          */
 
+        Laravel\Socialite\SocialiteServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -208,6 +210,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        //Custom
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+
+
 
     ],
 
